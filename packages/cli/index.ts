940d1027722d9a2b59ff5cfe0a051c0b9bb69179
Karim Shehadeh <kshehadeh@underarmour.com>
2024-01-06 10:13:51 -0500

    Minor changes

--- packages/cli/index.ts
+++ packages/cli/index.ts
@@ -1,6 +1,10 @@
 import yargs, { CommandModule } from 'yargs';
 import projectCommand from './src/commands/info';
 
+import '@hanto/plugin-git';
+import '@hanto/plugin-npm';
+import '@hanto/plugin-nextjs';
+
 yargs(process.argv.slice(2))
     .scriptName('hanto')
     .usage('$0 <cmd> [args]')
